549c156b394ccb5a8cd4adb0eb08016597e26ce8
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-06-30 23:21:36 +0600

    added basic routing

---
+++ src/app/contacts/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h2 className='bg-yellow-400 text-black'>This is Contacts page</h2>
+        </div>
+    );
+};
+
+export default ContactPage;
